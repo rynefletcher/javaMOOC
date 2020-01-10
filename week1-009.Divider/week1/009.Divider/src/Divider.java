a0e1ac2c4a91142401b23e3d377dc97ef5af2f95
rynefletcher <rynefletcher@hotmail.com>
2020-01-10 11:40:20 -0600

    Corrected code

--- week1-009.Divider/week1/009.Divider/src/Divider.java
+++ week1-009.Divider/week1/009.Divider/src/Divider.java
@@ -1,11 +1,16 @@
-
 import java.util.Scanner;
-
+ 
 public class Divider {
-
+ 
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
-
-        // Implement your program here. Remember to ask the input from user.
+ 
+        System.out.print("Type a number: ");
+        int dividend = Integer.parseInt(reader.nextLine());
+        System.out.print("Type another number: ");
+        int divisor = Integer.parseInt(reader.nextLine());
+        double quotient = (double) dividend / divisor;
+        
+        System.out.println("Division: " + dividend + " / " + divisor + " = " + quotient);
     }
 }
