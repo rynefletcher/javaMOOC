c7be232d493bdccec2059bfd34ea355d0ab8bca1
rynefletcher <rynefletcher@hotmail.com>
2020-01-10 11:59:49 -0600

    Corrected code

--- week2-035.SumOfThePowers/week2/035.SumOfThePowers/src/SumOfThePowers.java
+++ week2-035.SumOfThePowers/week2/035.SumOfThePowers/src/SumOfThePowers.java
@@ -1,10 +1,22 @@
-
 import java.util.Scanner;
-
+ 
 public class SumOfThePowers {
-
+ 
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
-
+        
+        System.out.println("Type a number:");
+        int userNum = Integer.parseInt(reader.nextLine());
+        
+        int sum = 0;
+        int exponential = 0;
+        
+        while(exponential <= userNum){
+            int result = (int)Math.pow(2, exponential);
+            sum += result;
+            exponential++;
+        }
+        
+        System.out.println("The result is " + sum);
     }
 }
