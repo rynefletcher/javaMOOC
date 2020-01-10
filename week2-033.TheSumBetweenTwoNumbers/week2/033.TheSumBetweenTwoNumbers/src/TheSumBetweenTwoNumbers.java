b981f0e8100558ca3911126ba32525a4a5f6a313
rynefletcher <rynefletcher@hotmail.com>
2020-01-10 11:58:41 -0600

    Corrected code

--- week2-033.TheSumBetweenTwoNumbers/week2/033.TheSumBetweenTwoNumbers/src/TheSumBetweenTwoNumbers.java
+++ week2-033.TheSumBetweenTwoNumbers/week2/033.TheSumBetweenTwoNumbers/src/TheSumBetweenTwoNumbers.java
@@ -1,10 +1,21 @@
-
 import java.util.Scanner;
-
-
+ 
+ 
 public class TheSumBetweenTwoNumbers {
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
         
+        System.out.println("First: ");
+        int userNum1 = Integer.parseInt(reader.nextLine());
+        System.out.println("Last: ");
+        int userNum2 = Integer.parseInt(reader.nextLine());
+        int sum = 0;
+        int counter = userNum1;
+        
+        while(counter <= userNum2){
+            sum += counter;
+            counter++;
+        }
+        System.out.println(sum);
     }
 }
