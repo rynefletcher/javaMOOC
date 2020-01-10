eaeb8515575d0c9e262bf64037824d145d21713f
rynefletcher <rynefletcher@hotmail.com>
2020-01-10 11:47:09 -0600

    Corrected code

--- week1-017.GreaterNumber/week1/017.GreaterNumber/src/GreaterNumber.java
+++ week1-017.GreaterNumber/week1/017.GreaterNumber/src/GreaterNumber.java
@@ -1,9 +1,23 @@
 import java.util.Scanner;
-
+ 
 public class GreaterNumber {
-
+ 
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
-
+        
+        System.out.println("Type a number: ");
+        int firstNum = Integer.parseInt(reader.nextLine());
+        System.out.println("Type another number: ");
+        int secNum = Integer.parseInt(reader.nextLine());
+        
+        if(firstNum > secNum){
+            System.out.println("Greater number: " + firstNum);
+        }
+        else if(secNum > firstNum){
+            System.out.println("Greater number: " + secNum);
+        }
+        else{
+            System.out.println("The numbers are equal!");
+        }
     }
 }
