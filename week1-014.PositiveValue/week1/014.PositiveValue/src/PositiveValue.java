2c551082b5760190f9c1d274c8d6110b479c8e44
rynefletcher <rynefletcher@hotmail.com>
2020-01-10 11:45:18 -0600

    Corrected code

--- week1-014.PositiveValue/week1/014.PositiveValue/src/PositiveValue.java
+++ week1-014.PositiveValue/week1/014.PositiveValue/src/PositiveValue.java
@@ -1,11 +1,18 @@
-
 import java.util.Scanner;
-
+ 
 public class PositiveValue {
-
+ 
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
-
-        // Type your program here:
+        
+        System.out.println("Type a number: ");
+        int userNum = Integer.parseInt(reader.nextLine());
+        
+        if(userNum > 0){
+            System.out.println("The number is positive.");
+        }
+        else{
+            System.out.println("The number is not positive.");
+        }
     }
 }
