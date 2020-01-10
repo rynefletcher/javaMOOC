3dfafee2cbebbd40d16728f8e06e787b97f2eb0f
rynefletcher <rynefletcher@hotmail.com>
2020-01-10 11:42:36 -0600

    Corrected code

--- week1-012.SumOfTheAges/week1/012.SumOfTheAges/src/SumOfTheAges.java
+++ week1-012.SumOfTheAges/week1/012.SumOfTheAges/src/SumOfTheAges.java
@@ -1,11 +1,22 @@
-
 import java.util.Scanner;
-
+ 
 public class SumOfTheAges {
-
+ 
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
-
-        // Implement your program here
+ 
+        System.out.print("Type your name: ");
+        String name = reader.nextLine();
+        
+        System.out.print("Type your age: ");
+        int age = Integer.parseInt(reader.nextLine());
+        
+        System.out.print("Type your name: ");
+        String secName = reader.nextLine();
+        
+        System.out.print("Type your age: ");
+        int secAge = Integer.parseInt(reader.nextLine());
+        
+        System.out.println(name + " and " + secName + " are " + (age + secAge) + " years old in total.");
     }
 }
