2d9b64f9ee5782d90f7cb2637e983e0718ecd378
rynefletcher <rynefletcher@hotmail.com>
2020-01-10 11:59:09 -0600

    Corrected code

--- week2-034.Factorial/week2/034.Factorial/src/Factorial.java
+++ week2-034.Factorial/week2/034.Factorial/src/Factorial.java
@@ -1,8 +1,23 @@
 import java.util.Scanner;
-
+ 
 public class Factorial {
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
-
+        
+        System.out.println("Type a number: ");
+        int userNum = Integer.parseInt(reader.nextLine());
+        int factorial = 1;
+        int counter = 1;
+        
+        if(userNum == 0){
+            System.out.println("Factorial is " + userNum);
+        }
+        else{
+            while(counter <= userNum){
+                factorial *= counter;
+                counter++;
+            }
+            System.out.println("Factorial is " + factorial);
+        }
     }
 }
