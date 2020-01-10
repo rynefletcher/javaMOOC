f54f5e642687143b6eed1791f9c02646e5a4b5c2
rynefletcher <rynefletcher@hotmail.com>
2020-01-10 11:57:19 -0600

    Corrected code

--- week2-030.UpToCertainNumber/week2/030.UpToCertainNumber/src/UpToCertainNumber.java
+++ week2-030.UpToCertainNumber/week2/030.UpToCertainNumber/src/UpToCertainNumber.java
@@ -1,13 +1,19 @@
-
 import java.util.Scanner;
-
-
+ 
+ 
 public class UpToCertainNumber {
-
+ 
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
         
-        // Write your code here
+        System.out.println("Up to what number? ");
+        int num = Integer.parseInt(reader.nextLine());
+        int counter = 1;
+        
+        while(counter <= num){
+            System.out.println(counter);
+            counter++;
+        }
         
     }
 }
