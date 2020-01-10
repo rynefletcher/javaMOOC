516ba253fa59fcf05018c11a91a8866bd397ebab
rynefletcher <rynefletcher@hotmail.com>
2020-01-10 11:55:17 -0600

    Corrected code

--- week2-027.FromOneToHundred/week2/027.FromOneToHundred/src/FromOneToHundred.java
+++ week2-027.FromOneToHundred/week2/027.FromOneToHundred/src/FromOneToHundred.java
@@ -1,7 +1,11 @@
-
 public class FromOneToHundred {
-
+ 
     public static void main(String[] args) {
-        // Write your program here
+        int num = 1;
+        
+        while(num <=100){
+            System.out.println(num);
+            num++;
+        }
     }
 }
