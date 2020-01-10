5e9e24f5508c01b929ee21b037b58dd85eb277de
rynefletcher <rynefletcher@hotmail.com>
2020-01-10 11:51:11 -0600

    Corrected code

--- week1-022.Password/week1/022.Password/src/Password.java
+++ week1-022.Password/week1/022.Password/src/Password.java
@@ -1,12 +1,21 @@
-
 import java.util.Scanner;
-
+ 
 public class Password {
-
+ 
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
         String password = "carrot"; // Use carrot as password when running tests.
-
-        // Write your code here
+        
+        System.out.println("Type the password: ");
+        String userPass = reader.nextLine();
+        
+        while(!password.equals(userPass)){
+            System.out.println("Wrong!");
+            System.out.println("Type the password: ");
+            userPass = reader.nextLine();
+        }
+        
+        System.out.println("Right!");
+        System.out.println("The secret is: jryy qbar!");
     }
 }
