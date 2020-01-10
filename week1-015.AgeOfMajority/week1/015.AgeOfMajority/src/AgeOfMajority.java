da6efa40307def19da0c9763522b233d4a344142
rynefletcher <rynefletcher@hotmail.com>
2020-01-10 11:46:05 -0600

    Corrected code

--- week1-015.AgeOfMajority/week1/015.AgeOfMajority/src/AgeOfMajority.java
+++ week1-015.AgeOfMajority/week1/015.AgeOfMajority/src/AgeOfMajority.java
@@ -1,11 +1,18 @@
-
 import java.util.Scanner;
-
+ 
 public class AgeOfMajority {
-
+ 
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
-
-        // Type your program here 
+ 
+        System.out.println("How old are you? ");
+        int userAge = Integer.parseInt(reader.nextLine());
+        
+        if(userAge >= 18){
+            System.out.println("You have reache the age of majority!");
+        }
+        else{
+            System.out.println("You have not reached the age of majority yet!");
+        }
     }
 }
