80735c938ac017b90c9a0788abffb62c8a7f09d4
rynefletcher <rynefletcher@hotmail.com>
2020-01-10 11:56:44 -0600

    Corrected code

--- week2-029.EvenNumbers/week2/029.EvenNumbers/src/EvenNumbers.java
+++ week2-029.EvenNumbers/week2/029.EvenNumbers/src/EvenNumbers.java
@@ -1,7 +1,11 @@
-
 public class EvenNumbers {
-
+ 
     public static void main(String[] args) {
-        // Write your code here
+        int num = 2;
+        
+        while(num <= 100){
+            System.out.println(num);
+            num = num + 2;
+        }
     }
 }
