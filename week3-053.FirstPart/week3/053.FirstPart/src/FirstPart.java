0e4deb7bfaca0b1a625695e5a8d42a491f88d336
rynefletcher <rynefletcher@hotmail.com>
2020-01-20 11:46:00 -0600

    Updated code

--- week3-053.FirstPart/week3/053.FirstPart/src/FirstPart.java
+++ week3-053.FirstPart/week3/053.FirstPart/src/FirstPart.java
@@ -1,9 +1,19 @@
-
 import java.util.Scanner;
 
 public class FirstPart {
 
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
+        System.out.println("Type a word: ");
+        String userWord = reader.nextLine();
+        System.out.println("Length of the first part: ");
+        int userLength = Integer.parseInt(reader.nextLine());
+        
+        System.out.print("Result: ");
+        int counter = 0;
+        while(counter < userLength){
+            System.out.print(userWord.charAt(counter));
+            counter++;
+        }
     }
 }
