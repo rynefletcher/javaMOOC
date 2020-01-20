06acc63888446dd2cf09cf0cb09aec22e8243cae
rynefletcher <rynefletcher@hotmail.com>
2020-01-20 10:27:02 -0600

    Updated Code

--- week3-050.SeparatingFirstCharacters/week3/050.SeparatingFirstCharacters/src/FirstCharacters.java
+++ week3-050.SeparatingFirstCharacters/week3/050.SeparatingFirstCharacters/src/FirstCharacters.java
@@ -3,5 +3,16 @@ import java.util.Scanner;
 public class FirstCharacters {
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
+        
+        System.out.println("Type your name: ");
+        String name = reader.nextLine();
+        int counter = 0;
+        
+        if (name.length() >= 3){
+            while (counter <= 2){
+                System.out.println((counter + 1) + ". character: " + name.charAt(counter));
+                counter++;
+            }
+        }
     }
 }
