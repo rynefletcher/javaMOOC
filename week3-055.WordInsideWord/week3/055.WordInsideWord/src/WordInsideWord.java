6410332b310a059be4531cd0f3fe81c80b91eb1b
rynefletcher <rynefletcher@hotmail.com>
2020-01-20 12:44:57 -0600

    Updated code

--- week3-055.WordInsideWord/week3/055.WordInsideWord/src/WordInsideWord.java
+++ week3-055.WordInsideWord/week3/055.WordInsideWord/src/WordInsideWord.java
@@ -1,9 +1,19 @@
-
 import java.util.Scanner;
 
 public class WordInsideWord {
 
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
+        System.out.println("Type the first word: ");
+        String firstWord = reader.nextLine();
+        System.out.println("Type the second word: ");
+        String secWord = reader.nextLine();
+        
+        if(firstWord.indexOf(secWord) >= 0){
+            System.out.println("The word '" + secWord + "' is found in the word '" + firstWord + "'.");
+        }
+        else{
+            System.out.println("The word '" + secWord + "' is not found in the word '" + firstWord + "'.");
+        }
     }
 }
