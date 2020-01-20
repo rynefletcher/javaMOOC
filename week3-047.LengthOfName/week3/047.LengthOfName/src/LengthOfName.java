cb5eb0f6dca280739d609359a34b417587814d9d
rynefletcher <rynefletcher@hotmail.com>
2020-01-20 08:30:23 -0600

    code update

--- week3-047.LengthOfName/week3/047.LengthOfName/src/LengthOfName.java
+++ week3-047.LengthOfName/week3/047.LengthOfName/src/LengthOfName.java
@@ -1,13 +1,19 @@
-
 import java.util.Scanner;
-
+ 
 public class LengthOfName {
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
+        System.out.println("Type your name: ");
+        String name = reader.nextLine();
         // call your method from here
+        int nameLength = 0;
+        nameLength = calculateCharacters(name);
+        System.out.println("Number of characters: " + nameLength);
     }
     
     // do here the method
-    // public static int calculateCharacters(String text)
+    public static int calculateCharacters(String text){
+        return text.length();
+    }
     
 }
