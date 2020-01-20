6ec5a4c9e27e9a84397bd98f7bac1c28ba27ed92
rynefletcher <rynefletcher@hotmail.com>
2020-01-19 21:11:09 -0600

    Added code

--- week3-043.SumOfNumbers/week3/043.SumOfNumbers/src/SumOfNumbers.java
+++ week3-043.SumOfNumbers/week3/043.SumOfNumbers/src/SumOfNumbers.java
@@ -1,12 +1,25 @@
 public class SumOfNumbers {
 
+ 
+
     public static int sum(int number1, int number2, int number3, int number4) {
+
         // write your code here
-        return -1;
+
+        int sum = number1 + number2 + number3 + number4;
+
+        return sum;
+
     }
 
+ 
+
     public static void main(String[] args) {
+
         int answer = sum(4, 3, 6, 1);
+
         System.out.println("Sum: " + answer);
+
     }
+
 }
