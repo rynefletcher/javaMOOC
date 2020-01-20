51fee6784e3b8870b3d7b11860c424ccb0754a7f
rynefletcher <rynefletcher@hotmail.com>
2020-01-20 10:46:23 -0600

    Updated Code

--- week3-052.ReversingName/week3/052.ReversingName/src/ReversingName.java
+++ week3-052.ReversingName/week3/052.ReversingName/src/ReversingName.java
@@ -3,5 +3,13 @@ import java.util.Scanner;
 public class ReversingName {
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
-    }
+        System.out.println("Type your name: ");
+        String name = reader.nextLine();
+        int counter = name.length();
+        
+        while (counter > 0){
+            System.out.print(name.charAt(counter - 1));
+            counter--;
+            }
+        }
 }
