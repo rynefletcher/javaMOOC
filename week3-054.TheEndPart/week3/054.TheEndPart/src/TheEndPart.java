aafc616d4f7b4e46fea2da7a8b2b5a418fffab17
rynefletcher <rynefletcher@hotmail.com>
2020-01-20 11:54:32 -0600

    Updated code

--- week3-054.TheEndPart/week3/054.TheEndPart/src/TheEndPart.java
+++ week3-054.TheEndPart/week3/054.TheEndPart/src/TheEndPart.java
@@ -3,5 +3,16 @@ import java.util.Scanner;
 public class TheEndPart {
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
+        System.out.println("Type a word: ");
+        String userWord = reader.nextLine();
+        System.out.println("Length of the end part: ");
+        int userLength = Integer.parseInt(reader.nextLine());
+        
+        System.out.print("Result: ");
+        int counter = userWord.length() - userLength;
+        while(counter < userWord.length()){
+            System.out.print(userWord.charAt(counter));
+            counter++;
+        }
     }
 }
