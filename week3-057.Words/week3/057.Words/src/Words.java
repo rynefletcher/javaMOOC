7f83756c89655df53804fb7f797d9f1d7cff1f34
rynefletcher <rynefletcher@hotmail.com>
2020-01-21 13:16:58 -0600

    Updated code

--- week3-057.Words/week3/057.Words/src/Words.java
+++ week3-057.Words/week3/057.Words/src/Words.java
@@ -5,5 +5,17 @@ public class Words {
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
         ArrayList<String> words = new ArrayList<String>();
+        
+        String userInput = " ";
+        while(!"".equals(userInput)){
+            System.out.print("Type a word: ");
+            userInput = reader.nextLine();
+            words.add(userInput);
+        }
+        
+        System.out.println("You typed the following words:");
+        for(String word : words){
+            System.out.println(word);
+        }
     }
 }
