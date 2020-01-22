5727f0b22eb8882539da0e0a96d3c246d2f00271
rynefletcher <rynefletcher@hotmail.com>
2020-01-22 10:21:57 -0600

    Updated code

--- week3-060.WordsInAlphabeticalOrder/week3/060.WordsInAlphabeticalOrder/src/WordsInAlphabeticalOrder.java
+++ week3-060.WordsInAlphabeticalOrder/week3/060.WordsInAlphabeticalOrder/src/WordsInAlphabeticalOrder.java
@@ -1,4 +1,3 @@
-
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Scanner;
@@ -8,5 +7,25 @@ public class WordsInAlphabeticalOrder {
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
         // create here an ArrayList
+        ArrayList<String> words = new ArrayList<String>();
+        
+        while(true){
+            String userInput = "";
+            System.out.println("Type a word: ");
+            userInput = reader.nextLine();
+            
+            if(userInput.equals("")){
+                break;
+            }
+            else{
+                words.add(userInput);
+            }
+        }
+        
+        Collections.sort(words);
+        
+        for(String word : words){
+            System.out.println(word);
+        }
     }
 }
