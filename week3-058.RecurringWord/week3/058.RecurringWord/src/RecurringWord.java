d13d31fa62689490608d1ea5a0676822f739f698
rynefletcher <rynefletcher@hotmail.com>
2020-01-22 10:09:03 -0600

    Updated Code

--- week3-058.RecurringWord/week3/058.RecurringWord/src/RecurringWord.java
+++ week3-058.RecurringWord/week3/058.RecurringWord/src/RecurringWord.java
@@ -1,4 +1,3 @@
-
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -7,6 +6,20 @@ public class RecurringWord {
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
         // create here the ArrayList 
+        ArrayList<String> words = new ArrayList<String>();
+        String userInput = "";
         
+        while(true){
+            System.out.print("Type a word: ");
+            userInput = reader.nextLine();
+            
+            if(words.contains(userInput)){
+                System.out.println("You gave the word " + userInput + " twice");
+                break;
+            }
+            else{
+                words.add(userInput);
+            }
+        }
     }
 }
