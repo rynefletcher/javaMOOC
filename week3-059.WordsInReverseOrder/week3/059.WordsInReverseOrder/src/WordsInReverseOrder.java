045e30a57fbcf0948285b4569d9d1a0e329044c6
rynefletcher <rynefletcher@hotmail.com>
2020-01-22 10:10:51 -0600

    Updated code

--- week3-059.WordsInReverseOrder/week3/059.WordsInReverseOrder/src/WordsInReverseOrder.java
+++ week3-059.WordsInReverseOrder/week3/059.WordsInReverseOrder/src/WordsInReverseOrder.java
@@ -6,5 +6,25 @@ public class WordsInReverseOrder {
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
         // create here an ArrayList
+        ArrayList<String> words = new ArrayList<String>();
+        
+        while(true){
+            String userInput = "";
+            System.out.println("Type a word: ");
+            userInput = reader.nextLine();
+            
+            if(userInput.equals("")){
+                break;
+            }
+            else{
+                words.add(userInput);
+            }
+        }
+        
+        Collections.reverse(words);
+        
+        for(String word : words){
+            System.out.println(word);
+        }
     }
 }
